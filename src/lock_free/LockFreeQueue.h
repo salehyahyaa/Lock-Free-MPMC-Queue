bd0bb9d9d67a292dd712c106c9a80a153e2d5589
Christopher Nicolas Fawaz <chrisfawaz53@gmail.com>
2026-04-26 14:39:51 -0500

    Update LockFreeQueue.h

--- src/lock_free/LockFreeQueue.h
+++ src/lock_free/LockFreeQueue.h
@@ -1,1 +1,31 @@
+#pragma once
 
+#include <atomic>
+#include <optional>
+
+template <typename T>
+class LockFreeQueue {
+public:
+    LockFreeQueue();
+    ~LockFreeQueue();
+
+    LockFreeQueue(const LockFreeQueue&) = delete;
+    LockFreeQueue& operator=(const LockFreeQueue&) = delete;
+
+    void push(T value);
+    std::optional<T> try_pop();
+    bool empty() const;
+
+private:
+    struct Node {
+        T data;
+        std::atomic<Node*> next;
+        Node() : next(nullptr) {}
+        Node(T val) : data(std::move(val)), next(nullptr) {}
+    };
+
+    std::atomic<Node*> head_;
+    std::atomic<Node*> tail_;
+};
+
+#include "LockFreeQueue.cpp"
